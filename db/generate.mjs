f6f85afcf632da532a90dcaee3e67967653ad9fd
Azizulloh <azizulloxsunatullayev@gmail.com>
2024-09-12 18:35:57 +0500

    first commit

---
+++ db/generate.mjs
@@ -0,0 +1,26 @@
+
+import {writeFile , readFile} from "fs/promises"
+import path from "path"
+import { fileURLToPath } from "url"
+import {faker} from "@faker-js/faker"
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const usersFilePath = path.join(__dirname , "users.json")
+
+const usersGenerator = async ()=>{
+    const data = [];
+    for(let i = 0 ; i < 100 ; i++){
+        const user ={
+            fullName : faker.person.fullName(), 
+            uuid : faker.string.uuid(),
+            email : faker.internet.email(),
+            phoneNumber : faker.phone.number(),
+            registerDate : faker.date.past(2),
+        }
+        data.push(user)
+    }
+    await writeFile(usersFilePath, JSON.stringify(data))
+}
+usersGenerator()
